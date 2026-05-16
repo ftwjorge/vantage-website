e1b8ef86c70ec2cfae9e0fb8d14e2e29e2a4dbd8
Jorge <javiking030406@icloud.com>
2026-05-16 18:36:19 -0400

    feat: add AOS scroll animations

---
+++ main.js
@@ -0,0 +1,7 @@
+// Initialize scroll animations
+AOS.init({
+  duration: 600,      // animation duration in ms
+  easing: 'ease-out', // easing function
+  once: true,         // only animate once per element
+  offset: 60          // px from bottom of viewport to trigger
+});
