f0141cb2ce91aa51f4690a41e2638b69263b68d4
Jorge <javiking030406@icloud.com>
2026-05-16 22:30:34 -0400

    feat: 9-service 3x3 grid, updated copy, who we work with rewrite, Calendly dark mode

--- main.js
+++ main.js
@@ -10,6 +10,6 @@ AOS.init({
 function openCalendly(e) {
   e.preventDefault();
   Calendly.initPopupWidget({
-    url: 'https://calendly.com/prof-jquevedo06/30min'
+    url: 'https://calendly.com/prof-jquevedo06/30min?background_color=0d0f12&text_color=ffffff&primary_color=c9a96e'
   });
 }
